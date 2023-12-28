5265503ef8000a2162cb643c825d6af0d98f3e5b
Erik Odiin <erik.odiin@antonsport.no>
2023-12-28 14:34:58 +0100

    modeled person from personschema

--- myApp.js
+++ myApp.js
@@ -4,8 +4,19 @@ const dotenv = require('dotenv').config()
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  age: {
+    type: Number
+  },
+  favoriteFoods: {
+    type: [String]
+  }
+})
 
-let Person;
+let Person = mongoose.model("Person", personSchema)
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
